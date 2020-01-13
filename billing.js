fb9a6d8ca2e7d05fec7697151b052422bc371716
Jimjule <roadknightj@gmail.com>
2020-01-13 11:18:16 +0000

    Moved code insde try block in billing

--- billing.js
+++ billing.js
@@ -3,14 +3,12 @@ import { calculateCost } from "./libs/billing-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
-  const { storage, source } = JSON.parse(event.body);
-  const amount = calculateCost(storage);
-  const description = "Scratch charge";
-
-  // Load our secret key from the  environment variables
-  const stripe = stripePackage(process.env.stripeSecretKey);
-
   try {
+    const { storage, source } = JSON.parse(event.body);
+    const amount = calculateCost(storage);
+    const description = "Scratch charge";
+    // Load our secret key from the  environment variables
+    const stripe = stripePackage(process.env.stripeSecretKey);
     await stripe.charges.create({
       source,
       amount,
